e6f56c45f9ba48ecc2d021b79267cfaa62362da1
Shubham Sharma <shubham@iastate.edu>
2019-03-01 01:16:30 -0600

    3.2.2 Part written up

--- lab3/t1.c
+++ lab3/t1.c
@@ -1,18 +1,18 @@
-/* t1.c 
-   use mutex to access critical area 
-   compile use: gcc -o t1 t1.c -lpthread 
+/* t1.c
+   use mutex to access critical area
+   compile use: gcc -o t1 t1.c -lpthread
 */
 
 #include <stdio.h>
 #include <pthread.h>
 
 /* define two routines called by threads*/
-void 	increment();	// increment global variable		    
+void 	increment();	// increment global variable
 void	decrement();	// decrement global variable
 
 
 /* global variable: mutex*/
-pthread_mutex_t mutex;  				
+pthread_mutex_t mutex;
 
 /*global variable: counter*/
 int v;
@@ -21,64 +21,68 @@ int v;
 int main (int argc, char *argv[])
 {
   /* thread id or type*/
-  pthread_t 	tid_increment;			
-  pthread_t	tid_decrement; 
-  
+  pthread_t 	tid_increment;
+  pthread_t	tid_decrement;
+
   /*initialize v as 0*/
   v = 0;
-  
+
   /* mutex initialization*/
-  pthread_mutex_init(&mutex, NULL);	
-  
+  pthread_mutex_init(&mutex, NULL);
+
   /* thread creation */
-  pthread_create(&tid_increment, NULL, (void*)&increment, NULL); 
-  pthread_create(&tid_decrement, NULL, (void*)&decrement, NULL); 
-  
+  pthread_create(&tid_increment, NULL, (void*)&increment, NULL);
+  pthread_create(&tid_decrement, NULL, (void*)&decrement, NULL);
+
   /* main waits for the two threads to finish */
-  pthread_join(tid_increment, NULL);  
+  pthread_join(tid_increment, NULL);
   pthread_join(tid_decrement, NULL);
-  
+
   printf("v=%d\n",v);
-  
+
   return 0;
 }
 
 
 
 void increment()
-{   
+{
   int	i;
   int a;
-  for( i = 0; i < 99; i++) 
-  {	
-    pthread_mutex_lock(&mutex);
-    
+  for( i = 0; i < 99; i++)
+  {
+    //REMOVED THIS LINE
+    //pthread_mutex_lock(&mutex);
+
     /* critical section */
     a = v + 10;
     usleep(1);
     v = a;
     /* end of critical section */
-    
-    pthread_mutex_unlock(&mutex);
+
+    //REMOVED THIS LINE
+    //pthread_mutex_unlock(&mutex);
   }
 }
 
 
 void decrement()
-{	
+{
   int  i;
   int  a;
-  
+
   for(i = 0; i < 99; i++)
-  {	
-    pthread_mutex_lock(&mutex);
-    
+  {
+    //REMOVED THIS LINE
+    //pthread_mutex_lock(&mutex);
+
     /* critical section */
     a = v - 10;
     usleep(2);
     v = a;
     /* end of critical section */
-    
-    pthread_mutex_unlock(&mutex);
+
+    //REMOVED THIS LINE
+    //pthread_mutex_unlock(&mutex);
   }
 }
--- lab3/t2.c
+++ lab3/t2.c
@@ -1,32 +1,37 @@
 /* t2.c
-   synchronize threads through mutex and conditional variable 
-   To compile use: gcc -o t2 t2.c -lpthread 
-*/ 
+   synchronize threads through mutex and conditional variable
+   To compile use: gcc -o t2 t2.c -lpthread
+*/
 
 #include <stdio.h>
 #include <pthread.h>
 
-void 	hello();    // define two routines called by threads    
-void 	world();         	
+void 	hello();    // define two routines called by threads
+void 	world();
+void  again();
 
 /* global variable shared by threads */
 pthread_mutex_t 	mutex;  		// mutex
 pthread_cond_t 		done_hello; 	// conditional variable
+pthread_cond_t    done_world;   // conditional variable
 int 			done = 0;      	// testing variable
 
 int main (int argc, char *argv[]){
-    pthread_t 	tid_hello, // thread id  
-    		tid_world; 
-    /*  initialization on mutex and cond variable  */ 
+    pthread_t 	tid_hello, // thread id
+    		tid_world, tid_again;
+    /*  initialization on mutex and cond variable  */
     pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&done_hello, NULL); 
-    
+    pthread_cond_init(&done_hello, NULL);
+    pthread_cond_init(&done_world, NULL);
+
     pthread_create(&tid_hello, NULL, (void*)&hello, NULL); //thread creation
-    pthread_create(&tid_world, NULL, (void*)&world, NULL); //thread creation 
+    pthread_create(&tid_world, NULL, (void*)&world, NULL); //thread creation
+    pthread_create(&tid_again, NULL, (void*)&again, NULL); //thread creation
 
     /* main waits for the two threads to finish */
-    pthread_join(tid_hello, NULL);  
+    pthread_join(tid_hello, NULL);
     pthread_join(tid_world, NULL);
+    pthread_join(tid_again, NULL);
 
     printf("\n");
     return 0;
@@ -34,7 +39,7 @@ int main (int argc, char *argv[]){
 
 void hello() {
     pthread_mutex_lock(&mutex);
-    printf("hello ");
+    printf("Hello ");
     fflush(stdout); 	// flush buffer to allow instant print out
     done = 1;
     pthread_cond_signal(&done_hello);	// signal world() thread
@@ -47,12 +52,26 @@ void world() {
     pthread_mutex_lock(&mutex);
 
     /* world thread waits until done == 1. */
-    while(done == 0) 
-	pthread_cond_wait(&done_hello, &mutex);
+    while(done == 0)
+	   pthread_cond_wait(&done_hello, &mutex);
 
-    printf("world");
+    printf("World ");
     fflush(stdout);
+    done = 1;
+    pthread_cond_signal(&done_world);	// signal world() thread
     pthread_mutex_unlock(&mutex); // unlocks mutex
 
+    return ;
+}
+
+void again() {
+    pthread_mutex_lock(&mutex);
+    /* world thread waits until done == 1. */
+    while(done == 0)
+      pthread_cond_wait(&done_world, &mutex);
+
+    printf("Again!");
+    fflush(stdout);
+    pthread_mutex_unlock(&mutex); // unlocks mutex
     return ;
 }
