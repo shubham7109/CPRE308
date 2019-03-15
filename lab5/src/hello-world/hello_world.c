e39c7fa81886cff1b2fbd85818dca646a42f0af4
shubham <shubham@iastate.edu>
2019-03-15 09:30:54 -0500

    Finished up lab 5

--- lab5/src/hello-world/hello_world.c
+++ lab5/src/hello-world/hello_world.c
@@ -1,11 +1,11 @@
 /**
  * @file      hello_world.c
- * @author    Jeramie Vens
- * @date      2015-03-08: Last updated
+ * @author    Shubham Sharma
+ * @date      2019-03-18: Last updated
  * @brief     Simple driver example
  * @copyright MIT License (c) 2015
  */
- 
+
 /*
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -20,18 +20,18 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #include <linux/kernel.h>
 #include <linux/init.h>
 
-#define DRIVER_AUTHOR "Jeramie Vens vens@iastate.edu"
-#define DRIVER_DESC   "A simple hello world driver"
+#define DRIVER_AUTHOR "Shubham Sharma shubham@iastate.edu"
+#define DRIVER_DESC   "A simple hello world driver for CPRE308"
 
 int __init hello_init(void)
 {
-	printk(KERN_INFO "Hello World!\n");
+	printk(KERN_INFO "Hello from init function!\n");
 	return 0;
 }
 
 void __exit hello_exit(void)
 {
-	printk(KERN_INFO "Goodbye World!\n");
+	printk(KERN_INFO "Goodbye from exit function!\n");
 }
 
 module_init(hello_init);
@@ -41,4 +41,3 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_SUPPORTED_DEVICE("testdevice");
-
